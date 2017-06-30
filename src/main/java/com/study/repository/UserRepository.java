30231e377acab718fef0e417026be8380f571c0c
sungjine456 <sungjine456@gmail.com>
2017-06-30 20:37:52 +0900

    JPA & H2 DB 추가

---
+++ src/main/java/com/study/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.study.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.study.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
