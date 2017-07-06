500a501ec6912450cbfa3ac2003ee6cbe8278cea
sungjine456 <sungjine456@gmail.com>
2017-07-06 17:43:45 +0900

    질문 상세보기, 수정, 삭제 기능 추가

--- src/main/java/com/study/web/QuestionController.java
+++ src/main/java/com/study/web/QuestionController.java
@@ -4,8 +4,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.study.domain.Question;
@@ -37,6 +41,32 @@ public class QuestionController {
 		System.out.println(sessionUser);
 		Question newQuestion = new Question(sessionUser, title, contents);
 		questionRepository.save(newQuestion);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable long id, Model model){
+		model.addAttribute("question",questionRepository.findOne(id));
+		return "/qna/show";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable long id, Model model){
+		model.addAttribute("question",questionRepository.findOne(id));
+		return "/qna/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable long id, String title, String contents) {
+		Question question = questionRepository.findOne(id);
+		question.update(title, contents);
+		questionRepository.save(question);
+		return String.format("redirect:/questions/%d", id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable long id) {
+		questionRepository.delete(id);
 		return "redirect:/";
 	}
 }
